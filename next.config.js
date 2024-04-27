4bdd2bf14735a42749fe24874cb93cdfcaf37d72
Alan P Currie <alanc909@outlook.com>
2024-04-27 10:41:19 +0100

    ci: :green_heart: ci

--- next.config.js
+++ next.config.js
@@ -1,26 +1,22 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {};
-
 module.exports = {
-    ...nextConfig,
-    async rewrites() {
-        return {
-            beforeFiles: [
-                {
-                    source: '/((?!.*\\.swa).*)',
-                    destination: '/',
-                }
-            ]
-        }
-    },
     async redirects() {
         return [
             {
-                source: '/((?!.*\\.swa).*)',
-                destination: '/',
+                source: '/((?!\\.swa).*)',
+                destination: 'https://delightful-beach-0001a6110.5.azurestaticapps.net/',
                 permanent: false,
             },
         ]
     },
+    async rewrites() {
+        return {
+            beforeFiles: [
+                {
+                    source: '/((?!\\.swa).*)',
+                    destination: 'https://delightful-beach-0001a6110.5.azurestaticapps.net/',
+                }
+            ]
+        }
+    },
     output: "standalone",
-}
+};
