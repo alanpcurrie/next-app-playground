a83f29a92017fa559cbb16ed2e4be53aaa44e283
Alan P Currie <alanc909@outlook.com>
2024-04-27 09:48:42 +0100

    feat: :sparkles: add az swa config

---
+++ middleware.ts
@@ -0,0 +1,10 @@
+export const config = {
+    matcher: [
+      /*
+       * Match all request paths except for the ones starting with:
+       * - .swa (Azure Static Web Apps)
+       */
+      '/((?!.swa).*)',
+    ],
+  }
+  
--- next.config.js
+++ next.config.js
@@ -1,4 +1,26 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {};
 
-module.exports = nextConfig;
+module.exports = {
+    ...nextConfig,
+    async rewrites() {
+        return {
+            beforeFiles: [
+                {
+                    source: '/((?!.swa).*)<YOUR MATCHING RULE>',
+                    destination: '<YOUR REWRITE RULE>',
+                }
+            ]
+        }
+    },
+    async redirects() {
+        return [
+            {
+                source: '/((?!.swa).*)<YOUR MATCHING RULE>',
+                destination: '<YOUR REDIRECT RULE>',
+                permanent: false,
+            },
+        ]
+    },
+    output: "standalone",
+}
