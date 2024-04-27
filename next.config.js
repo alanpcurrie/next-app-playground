ae473d9659afa32a939b55ddf2a18db94111db38
Alan P Currie <alanc909@outlook.com>
2024-04-27 10:01:16 +0100

    fix: :rocket: config

--- next.config.js
+++ next.config.js
@@ -3,24 +3,24 @@ const nextConfig = {};
 
 module.exports = {
     ...nextConfig,
-    async rewrites() {
-        return {
-            beforeFiles: [
-                {
-                    source: '/((?!.swa).*)<YOUR MATCHING RULE>',
-                    destination: '<YOUR REWRITE RULE>',
-                }
-            ]
-        }
-    },
-    async redirects() {
-        return [
-            {
-                source: '/((?!.swa).*)<YOUR MATCHING RULE>',
-                destination: '<YOUR REDIRECT RULE>',
-                permanent: false,
-            },
-        ]
-    },
+    // async rewrites() {
+    //     return {
+    //         beforeFiles: [
+    //             {
+    //                 source: '/((?!.swa).*)<YOUR MATCHING RULE>',
+    //                 destination: '<YOUR REWRITE RULE>',
+    //             }
+    //         ]
+    //     }
+    // },
+    // async redirects() {
+    //     return [
+    //         {
+    //             source: '/((?!.swa).*)<YOUR MATCHING RULE>',
+    //             destination: '<YOUR REDIRECT RULE>',
+    //             permanent: false,
+    //         },
+    //     ]
+    // },
     output: "standalone",
 }
