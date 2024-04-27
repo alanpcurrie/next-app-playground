54afa7e2bfa56b043b0a8e74f2e3202c28e886e0
Alan P Currie <alanc909@outlook.com>
2024-04-27 10:24:05 +0100

    ci: :rocket: ci

--- next.config.js
+++ next.config.js
@@ -8,7 +8,7 @@ module.exports = {
             beforeFiles: [
                 {
                     source: '/((?!.*\\.swa).*)',
-                    destination: 'https://delightful-beach-0001a6110.5.azurestaticapps.net/$1',
+                    destination: 'https://delightful-beach-0001a6110.5.azurestaticapps.net',
                 }
             ]
         }
@@ -17,7 +17,7 @@ module.exports = {
         return [
             {
                 source: '/((?!.*\\.swa).*)',
-                destination: 'https://delightful-beach-0001a6110.5.azurestaticapps.net/$1',
+                destination: 'https://delightful-beach-0001a6110.5.azurestaticapps.net',
                 permanent: false,
             },
         ]
