a4fdc1993fd158d38f5c34cb74b7a44da7becc9d
Alan P Currie <alanc909@outlook.com>
2024-06-11 16:41:11 +0100

    fix: :bug: try catch for o11y helper

--- components/observability.tsx
+++ components/observability.tsx
@@ -4,14 +4,13 @@ import { getWebAutoInstrumentations } from '@opentelemetry/auto-instrumentations
 import { HoneycombWebSDK } from '@honeycombio/opentelemetry-web';
 
 export default function Observability() {
-  const sdk = new HoneycombWebSDK({
-    debug: true, // turn off in production
-    apiKey: process.env.NEXT_PUBLIC_HNY_API_KEY, // Replace with your Honeycomb Ingest API Key
-    serviceName: 'next o11y poc', // Replace with your application name. Honeycomb will name your dataset using this variable.
-    instrumentations: [getWebAutoInstrumentations()], // Adds automatic instrumentation
-    endpoint: 'https://api.eu1.honeycomb.io/v1/traces', // uncomment to send to EU instance
-  });
-  sdk.start();
-
-  return null;
+  try {
+    const sdk = new HoneycombWebSDK({
+      debug: true, // turn off in production
+      apiKey: process.env.HNY_API_KEY, // Replace with your Honeycomb Ingest API Key
+      serviceName: 'next o11y poc', // Replace with your application name. Honeycomb will name your dataset using this variable.
+      instrumentations: [getWebAutoInstrumentations()], // Adds automatic instrumentation
+    });
+    sdk.start();
+  } catch (e) {}
 }
