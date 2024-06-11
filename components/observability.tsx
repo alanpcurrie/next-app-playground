1250e898cc4691f476da4a6d39069e79023c0fce
Alan P Currie <alanc909@outlook.com>
2024-06-11 16:51:44 +0100

    feat: :bug: added use effect for o11y

--- components/observability.tsx
+++ components/observability.tsx
@@ -1,16 +1,20 @@
 'use client'; // browser only: https://react.dev/reference/react/use-client
 
+import { useEffect } from 'react';
 import { getWebAutoInstrumentations } from '@opentelemetry/auto-instrumentations-web';
 import { HoneycombWebSDK } from '@honeycombio/opentelemetry-web';
 
 export default function Observability() {
-  try {
+  useEffect(() => {
     const sdk = new HoneycombWebSDK({
       debug: true, // turn off in production
+      // endpoint: "https://api.eu1.honeycomb.io/v1/traces", // uncomment to send to EU instance
       apiKey: process.env.HNY_API_KEY, // Replace with your Honeycomb Ingest API Key
       serviceName: 'next o11y poc', // Replace with your application name. Honeycomb will name your dataset using this variable.
-      instrumentations: [getWebAutoInstrumentations()], // Adds automatic instrumentation
+      instrumentations: [getWebAutoInstrumentations()],
     });
     sdk.start();
-  } catch (e) {}
+  }, []);
+
+  return null;
 }
